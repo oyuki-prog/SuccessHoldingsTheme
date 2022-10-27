d418d5e86eaad207def7afc321867a576fffd528
yukodajima <koda12131107@gmail.com>
2022-10-28 07:47:09 +0900

    ハンバーガーメニュー作成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,19 @@
 jQuery(function ($) {
-  $(window).on('load', function () {
+  $(window).on("load", function () {
+    const hamberger = $("#js-humberger");
+    const nav = $("#js-nav");
+    const fadein = ".fadein";
 
+    $hamMenu = hamberger.on("click", function () {
+      hamberger.toggleClass("active");
+      nav.toggleClass("active");
+      $(fadein).each(function (i) {
+        $(this)
+          .delay(i * 200)
+          .queue(function () {
+            $(this).addClass("active");
+          });
+      });
+    });
   });
 });
