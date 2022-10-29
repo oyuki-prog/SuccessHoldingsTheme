edb782c42345346eb740c6c4ae31018fab613c10
yukodajima <koda12131107@gmail.com>
2022-10-30 06:45:22 +0900

    FB項目の修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,22 @@
-jQuery(function ($) {
-  $(window).on("load", function () {
-    const hamberger = $("#js-humberger");
+$(window).on("load", function () {
+  jQuery(function ($) {
+    const hamburger = $("#js-hamburger");
     const nav = $("#js-nav");
     const fadein = $(".fadein");
+    const line = $(".c-hamburger__line");
+    const fixed = $(".fix");
 
-    $hamMenu = hamberger.on("click", function () {
-      hamberger.toggleClass("active");
+    hamburger.on("click", function () {
+      hamburger.toggleClass("active");
       nav.toggleClass("active");
+      line.toggleClass("active");
+      fixed.toggleClass("active");
 
-      fadein.each(function (i) {
-        $(this)
-          .delay(i * 200)
-          .queue(function () {
-            console.log("class: ", $(this).hasClass("active"));
-            $(this).toggleClass("active");
-          });
+      fadein.each(function (i, element) {
+        setTimeout(function () {
+          console.log("$(element)f: ", $(element));
+          $(element).toggleClass("active");
+        }, i * 200);
       });
     });
   });
