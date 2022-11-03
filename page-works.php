36e4c2b00459e03d913cc6d024b27d11fedb678a
Ishibashi Kentaro <gt.shibaken.01@gmail.com>
2022-11-03 18:00:20 +0900

    page-works php化

---
+++ page-works.php
@@ -0,0 +1,76 @@
+<?php get_header(); ?>
+<?php get_template_part('hero'); ?>
+      <section class="p-works">
+        <div class="l-container">
+          <ul class="p-works__list">
+            <li class="p-works__listItem">
+              <a href="">
+                <div class="p-works__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt=""></div>
+                <div class="p-works__hoverBox">
+                  <div class="p-works__textBox">
+                    <p class="p-works__companyName">株式会社〇〇〇〇</p>
+                    <p class="p-works__text">新人研修</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="p-works__listItem">
+              <a href="">
+                <div class="p-works__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt=""></div>
+                <div class="p-works__hoverBox">
+                  <div class="p-works__textBox">
+                    <p class="p-works__companyName">株式会社〇〇〇〇</p>
+                    <p class="p-works__text">新人研修</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="p-works__listItem">
+              <a href="">
+                <div class="p-works__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt=""></div>
+                <div class="p-works__hoverBox">
+                  <div class="p-works__textBox">
+                    <p class="p-works__companyName">株式会社〇〇〇〇</p>
+                    <p class="p-works__text">新人研修</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="p-works__listItem">
+              <a href="">
+                <div class="p-works__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt=""></div>
+                <div class="p-works__hoverBox">
+                  <div class="p-works__textBox">
+                    <p class="p-works__companyName">株式会社〇〇〇〇</p>
+                    <p class="p-works__text">新人研修</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="p-works__listItem">
+              <a href="">
+                <div class="p-works__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt=""></div>
+                <div class="p-works__hoverBox">
+                  <div class="p-works__textBox">
+                    <p class="p-works__companyName">株式会社〇〇〇〇</p>
+                    <p class="p-works__text">新人研修</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+            <li class="p-works__listItem">
+              <a href="">
+                <div class="p-works__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt=""></div>
+                <div class="p-works__hoverBox">
+                  <div class="p-works__textBox">
+                    <p class="p-works__companyName">株式会社〇〇〇〇</p>
+                    <p class="p-works__text">新人研修</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+<?php get_footer(); ?>
