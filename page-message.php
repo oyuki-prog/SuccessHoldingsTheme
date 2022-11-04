4d16bf91ab9178ed4ec7ebf06fd801aaa533d6c7
Saki Fukumoto <saki.fukumoto9918@gmail.com>
2022-11-04 22:38:44 +0900

    hero部分PHP化

--- page-message.php
+++ page-message.php
@@ -1,12 +1,5 @@
 <?php get_header(); ?>
-    <section class="c-hero">
-        <div class="c-hero__imgWrapper">
-          <img class="c-hero__img" src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg" alt="">
-        </div>
-        <div class="c-hero__container">
-          <h1 class="c-hero__mainTitle">メッセージ</h1>
-          <p class="c-hero__subTitle">Message</p>
-        </div>
+<?php get_template_part('hero'); ?>
       </section>
       <section class="p-message">
         <div class="l-container">
--- page-news.php
+++ page-news.php
@@ -1,16 +1,5 @@
 <?php get_header(); ?>
-      <section class="c-hero">
-        <div class="c-hero__imgWrapper">
-          <img
-            class="c-hero__img"
-            src="<?php echo get_template_directory_uri(); ?>/assets/images/img-sample.jpg"
-            alt=""
-          />
-        </div>
-        <div class="c-hero__container">
-          <h1 class="c-hero__mainTitle">お知らせ</h1>
-          <p class="c-hero__subTitle">News</p>
-        </div>
+<?php get_template_part('hero'); ?>
       </section>
       <section class="p-news">
         <div class="l-container">
