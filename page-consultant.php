17915b08cf074415f5e2fb6d7d36e593df4ed016
Ishibashi Kentaro <gt.shibaken.01@gmail.com>
2022-11-05 20:34:05 +0900

    php

--- page-consultant.php
+++ page-consultant.php
@@ -6,16 +6,15 @@
     <section class="p-serviceShow">
       <div class="l-container">
         <div class="p-serviceShow__container">
-          <h2 class="p-serviceShow__title"><?php echo post_custom("事業内容タイトル"); ?></h2>
+          <h2 class="p-serviceShow__title">事業内容</h2>
           <p class="p-serviceShow__text"><?php echo post_custom("事業内容テキスト"); ?></p>
         </div>
         <div class="p-serviceShow__container">
-          <h2 class="p-serviceShow__title"><?php echo post_custom("事業詳細タイトル"); ?></h2>
+          <h2 class="p-serviceShow__title">事業詳細</h2>
           <p class="p-serviceShow__text"><?php echo post_custom("事業詳細テキスト"); ?></p>
         </div>
       </div>
     </section>
   <?php endwhile; ?>
 <?php endif; ?>
-</main>
 <?php get_footer(); ?>
