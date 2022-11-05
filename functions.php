23b46242a5854523902a9351c3fdaea173353de2
Saki Fukumoto <saki.fukumoto9918@gmail.com>
2022-11-05 13:52:38 +0900

    hero部分タイトル修正

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 function display_title() {
     if (is_page()) {
         echo (get_the_title());
-    }elseif (is_single() && is_category('works')) {
+    } elseif (is_single() && is_category('works')) {
         echo '実績詳細';
-    }elseif (is_single() && is_category('news')) {
+    } elseif (is_single() && is_category('news')) {
         echo 'お知らせ';
     }
 }
@@ -12,17 +12,18 @@ function display_title() {
 function display_subTitle() {
     $page = get_post( get_the_ID() );
     $slug = $page->post_name;
+    $upper_camel_slug = ucfirst($slug);
 
     if (is_page() && $slug === 'aboutus') { //固定ページかつ戻り値のスラッグがaboutusだったら
         echo 'About Us';
-    } elseif (is_page()) { //固定ページならサブタイトルをecho
-        echo $slug;
-    } elseif (is_single() && is_category('works')) {//投稿ページかつカテゴリーがworksなら
-        echo 'Works';
-    } elseif (is_single() && is_category('news')) {//投稿ページかつカテゴリーがnewsなら
-        echo 'News';
+    } else { //固定ページならサブタイトルをecho
+        echo "$upper_camel_slug";
     }
 }
-?>
-<?php 
+
+function display_tabTitle() {
+    if (is_page()) {
+        echo get_the_title() . " | 株式会社サクセスホールディングス";
+    }
+}
 add_theme_support('post-thumbnails');
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>About us</title>
+  <title><?php display_tabTitle(); ?></title>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
 <?php wp_head(); ?>
 </head>
