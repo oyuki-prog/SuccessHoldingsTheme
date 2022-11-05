190412f18224edfc65b7199d5f3dd1cb5c2a950c
Saki Fukumoto <saki.fukumoto9918@gmail.com>
2022-11-05 20:14:52 +0900

    newsのphp修正

--- page-news.php
+++ page-news.php
@@ -4,25 +4,25 @@
 <section class="p-news">
   <div class="l-container">
     <ul class="">
-      <li class="p-news__container">
-        <?php
-        $args = array(
-          'post_type' => 'post',
-          'posts_per_page' => 12,
-          'category_name' => 'news',
-        );
-        $the_query = new WP_Query($args);
-        ?>
-        <?php if ($the_query->have_posts()) : ?>
-          <?php while ($the_query->have_posts()) : ?>
-            <?php $the_query->the_post(); ?>
+      <?php
+      $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => 12,
+        'category_name' => 'news',
+      );
+      $the_query = new WP_Query($args);
+      ?>
+      <?php if ($the_query->have_posts()) : ?>
+        <?php while ($the_query->have_posts()) : ?>
+          <?php $the_query->the_post(); ?>
+          <li class="p-news__container">
             <time class="p-news__date"><?php the_time('Y-m-d'); ?></time>
             <p class="p-news__text">
-              <a href="#"><?php the_title(); ?></a>
+              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
             </p>
-      </li>
-    <?php endwhile; ?>
-  <?php endif; ?>
+          </li>
+        <?php endwhile; ?>
+      <?php endif; ?>
     </ul>
   </div>
 </section>
