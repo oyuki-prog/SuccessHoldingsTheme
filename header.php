fbe91e3307402cebbeb6248b26f4578836e6633c
Saki Fukumoto <saki.fukumoto9918@gmail.com>
2022-11-05 10:41:46 +0900

    headerリンクPHP化

--- header.php
+++ header.php
@@ -29,22 +29,22 @@
         <nav class="p-header__nav" id="js-nav">
           <ul class="p-header__navList nav__items nav-items">
             <li class="p-header__items fadein">
-              <a class="p-headerLink c-link" href="service">サービス</a>
+              <a class="p-headerLink c-link" href="<?php echo home_url("/service"); ?>">サービス</a>
             </li>
             <li class="p-header__items fadein">
-              <a class="p-headerLink c-link" href="works">導入事例</a>
+              <a class="p-headerLink c-link" href="<?php echo home_url("/work"); ?>">導入事例</a>
             </li>
             <li class="p-header__items fadein">
-              <a class="p-headerLink c-link" href="news">ニュース</a>
+              <a class="p-headerLink c-link" href="<?php echo home_url("/news"); ?>">ニュース</a>
             </li>
             <li class="p-header__items fadein">
-              <a class="p-headerLink c-link" href="aboutUs">会社情報</a>
+              <a class="p-headerLink c-link" href="<?php echo home_url("/aboutUs"); ?>">会社情報</a>
             </li>
             <li class="p-header__items fadein">
-              <a class="p-headerLink c-link" href="recruit">採用情報</a>
+              <a class="p-headerLink c-link" href="<?php echo home_url("/recruit"); ?>">採用情報</a>
             </li>
             <li class="p-header__contact fadein">
-              <a class="p-header__contactBtn c-btn" href="confirm"
+              <a class="p-header__contactBtn c-btn" href="<?php echo home_url("/confirm"); ?>"
                 >お問い合わせ
                 </a>
             </li>
