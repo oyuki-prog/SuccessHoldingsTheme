e30c80f15494744f9e95d405a2557f493ed0aed2
Ishibashi Kentaro <gt.shibaken.01@gmail.com>
2022-12-04 02:52:44 +0900

    loading 実装

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,21 @@
-$(window).on("load", function () {
-  jQuery(function ($) {
+jQuery(function ($) {
+  // loader表示
+  $('body').append(
+    '<div class="c-loading"><div class="c-loading__origin"></div><p class="c-loadeing__text">Loading...</p></div>'
+  );
+  $(window).on("load", function () {
+    // loader非表示
+    // setTimeout(() => {
+    //   $('.c-loading').addClass('c-loading__completed');
+    // }, 1000);
     const hamburger = $("#js-hamburger");
     const fadein = $(".fadein");
     // const triger = $(".triger");
     const body = $("body");
-
+    
     hamburger.on("click", function () {
       body.toggleClass("active");
-
+      
       fadein.each(function (i, element) {
         setTimeout(function () {
           $(element).toggleClass("active");
