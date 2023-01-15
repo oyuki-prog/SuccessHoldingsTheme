1bfb4c1a1e3710e13e15ed476b2e460d09f7c3a2
Ishibashi Kentaro <gt.shibaken.01@gmail.com>
2023-01-16 03:06:44 +0900

    ローディング修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,37 @@
-$(window).on("load", function () {
-  jQuery(function ($) {
+$(function() {
   // loader表示
   $('body').append(
-    '<div class="c-loading"><div class="c-loading__origin"></div><p class="c-loading__text">Loading...</p></div>'
+  '<div class="c-loading"><div class="c-loading__origin"></div><p class="c-loading__text">Loading...</p></div>'
   );
+
+  $(window).on("load", function () {
     // loader非表示
-    $('.c-loading').addClass('c-loading__completed');
+    setTimeout(() => {
+      $('.c-loading').addClass('c-loading__completed');
+    }, 1000);
+
+    // ハンバーガーメニューの切り替え
     const hamburger = $("#js-hamburger");
     const fadein = $(".fadein");
-    // const triger = $(".triger");
     const body = $("body");
-    
     hamburger.on("click", function () {
       body.toggleClass("active");
-      
       fadein.each(function (i, element) {
         setTimeout(function () {
           $(element).toggleClass("active");
         }, i * 200);
       });
     });
-    // ニュースティッカー
-    $('.p-topNews__list').bxSlider({
-      touchEnabled:false,
-      mode: 'vertical',
-      controls: false,
-      auto: 'true',
-      pager: false
+
+    // ニュースティッカー(wpとの相性のためjQuery再読み込み)
+    jQuery(function($){
+      $('.p-topNews__list').bxSlider({
+        touchEnabled:false,
+        mode: 'vertical',
+        controls: false,
+        auto: 'true',
+        pager: false
+      });
     });
   });
 });
