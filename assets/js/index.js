1f653639475ffbc2dc3cec85582529d8d7ffc4c4
Ishibashi Kentaro <gt.shibaken.01@gmail.com>
2023-01-22 17:48:15 +0900

    ローディング初訪時のみ対応

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,29 @@
-jQuery(function ($) {
+$(function() {
   // loader表示
-  $('body').append(
+  const URL = location.href
+  if (!sessionStorage.getItem(URL)) {
+    //初回アクセス時の処理
+    sessionStorage.setItem(URL, 0);
+    $('body').append(
     '<div class="c-loading"><div class="c-loading__origin"></div><p class="c-loading__text">Loading...</p></div>'
-  );
+    );
+  }
+
   $(window).on("load", function () {
     // loader非表示
-    $('.c-loading').addClass('c-loading__completed');
+    setTimeout(() => {
+      $('.c-loading').addClass('c-loading__completed');
+    }, 1000);
+
+    // ハンバーガーメニューの切り替え
     const hamburger = $("#js-hamburger");
     const fadein = $(".fadein");
     // const triger = $(".triger");
     const body = $("body");
-    
+
     hamburger.on("click", function () {
       body.toggleClass("active");
-      
+
       fadein.each(function (i, element) {
         setTimeout(function () {
           $(element).toggleClass("active");
