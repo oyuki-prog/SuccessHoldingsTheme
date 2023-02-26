933bc2338873310ec0f9a2ac8ad9ea88a155a22b
shibaken0124 <gt.shibaken.01@gmail.com>
2023-02-27 00:04:35 +0900

    ニュースティッカーのはみ出し修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,33 @@
-$(function() {
-  // loader表示
-  const URL = location.href
-  if (!sessionStorage.getItem('access')) {
-    //初回アクセス時の処理
-    sessionStorage.setItem('access', 0);
-    $('body').append(
-    '<div class="c-loading"><div class="c-loading__origin"></div><p class="c-loading__text">Loading...</p></div>'
-    );
-  }
+$(window).on("load", function () {
+  // ハンバーガーメニューの切り替え
+  const hamburger = $("#js-hamburger");
+  const fadein = $(".fadein");
+  const body = $("body");
 
-  $(window).on("load", function () {
-    // loader非表示
-    setTimeout(() => {
-      $('.c-loading').addClass('c-loading__completed');
-    }, 1000);
+  hamburger.on("click", function () {
+    body.toggleClass("active");
 
-    // ハンバーガーメニューの切り替え
-    const hamburger = $("#js-hamburger");
-    const fadein = $(".fadein");
-    const body = $("body");
-
-    hamburger.on("click", function () {
-      body.toggleClass("active");
-
-      fadein.each(function (i, element) {
-        setTimeout(function () {
-          $(element).toggleClass("active");
-        }, i * 200);
-      });
-    });
-    console.log($(".slider"));
-    console.log($(".p-topNews__listItem"));
-    $(".slider").slick({
-      autoplay: true,
-      autoplaySpeed: 3000,
-      vertical: true,
-      verticalSwiping: true,
-      arrows: false,
+    fadein.each(function (i, element) {
+      setTimeout(function () {
+        $(element).toggleClass("active");
+      }, i * 200);
     });
   });
+
+  $(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+  });
+
+  //以下のコードはニュースティッカーを実装する際の枠内に文字が収まらないときに3点リーダーを実装するアイデア。完全にはレスポンシブに対応できないためボツ
+  // $(".p-topNews__link").each(function() {
+  //   var max_length = 25; // ここで制限したい文字数を指定する
+  //   if($(this).text().length > max_length) {
+  //     var truncated = $(this).text().substr(0,max_length) + '...';
+  //     $(this).text(truncated);
+  //   }
+  // });
 });
