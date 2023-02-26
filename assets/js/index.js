4d6d7a518fc4ad161666397d32d09fbd7a726b9a
shibaken0124 <gt.shibaken.01@gmail.com>
2023-02-26 17:14:55 +0900

    ローディングのjsを削除

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,17 @@
-$(function() {
-  // loader表示
-  const URL = location.href
-  if (!sessionStorage.getItem('access')) {
-    //初回アクセス時の処理
-    sessionStorage.setItem('access', 0);
-    $('body').append(
-    '<div class="c-loading"><div class="c-loading__origin"></div><p class="c-loading__text">Loading...</p></div>'
-    );
-  }
+$(window).on("load", function () {
+  // ハンバーガーメニューの切り替え
+  const hamburger = $("#js-hamburger");
+  const fadein = $(".fadein");
+  // const triger = $(".triger");
+  const body = $("body");
 
-  $(window).on("load", function () {
-    // loader非表示
-    setTimeout(() => {
-      $('.c-loading').addClass('c-loading__completed');
-    }, 1000);
+  hamburger.on("click", function () {
+    body.toggleClass("active");
 
-    // ハンバーガーメニューの切り替え
-    const hamburger = $("#js-hamburger");
-    const fadein = $(".fadein");
-    // const triger = $(".triger");
-    const body = $("body");
-
-    hamburger.on("click", function () {
-      body.toggleClass("active");
-
-      fadein.each(function (i, element) {
-        setTimeout(function () {
-          $(element).toggleClass("active");
-        }, i * 200);
-      });
+    fadein.each(function (i, element) {
+      setTimeout(function () {
+        $(element).toggleClass("active");
+      }, i * 200);
     });
   });
 });
